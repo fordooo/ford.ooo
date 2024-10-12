4bb9daf0624aa1f7132149b366c77f18f0a9a589
Aaron Ford <aaron@aaronford.net>
2024-10-12 16:06:35 -0700

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,89 @@
-import Image from "next/image";
+const ShadowFill = ({ character }: { character: string }) => {
+  return (
+    <>
+      {Array(24)
+        .fill('')
+        .map((_, i) => (
+          <span key={`row-${character}-${i}`}>{character.repeat(60)}</span>
+        ))}
+    </>
+  )
+}
+
+const ASCIIShadows = () => {
+  return (
+    <>
+      <div
+        aria-hidden="true"
+        className="font-shadow absolute left-5 top-5 -z-10 hidden h-full w-full flex-col overflow-hidden bg-gray-800 leading-[26px] tracking-[0.5px] text-gray-300 sm:flex"
+      >
+        <ShadowFill character="▒" />
+      </div>
+      <div
+        aria-hidden="true"
+        className="font-shadow absolute left-5 top-5 -z-20 flex h-full w-full flex-col overflow-hidden bg-gray-800 leading-[26px] tracking-[0.5px] text-gray-300 sm:left-10 sm:top-10 sm:text-gray-400"
+      >
+        <ShadowFill character="░" />
+      </div>
+    </>
+  )
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex min-h-dvh flex-col items-center justify-center bg-gray-900 p-8 font-mono">
+      <section className="relative z-10 mb-5 mr-5 flex max-w-[550px] flex-col bg-black sm:mb-10 sm:mr-10 lg:mr-0">
+        <div className="flex items-center justify-center gap-2 border-4 border-white bg-black p-2 text-black">
+          <div className="flex-1 gap-2 bg-white">
+            <h1 className="px-2 py-1 text-lg font-bold leading-5">
+              aaron_ford
+            </h1>
+          </div>
+          <div className="bg-white">
+            <p className="px-2 py-1 text-lg font-bold leading-5">×</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className="flex flex-1 flex-col gap-4 border-4 border-t-0 border-white bg-black px-4 py-8 text-sm">
+          <p>
+            Howdy! I&apos;m Aaron, a front-end developer and all-around web
+            enthusiast living in the Pacific Northwest.
+          </p>
+          <p>
+            I&apos;m currently working at Machobear Studios on exciting projects
+            like liv.rent. I love building with HTML, CSS, JavaScript,
+            TypeScript, React, and Next.js — but I&apos;m always eager to learn
+            new things.
+          </p>
+          <p className="mt-4">Feel free to connect:</p>
+          <div className="flex items-center gap-2">
+            <a
+              className="transition-transform-colors duration-300 hover:-rotate-3 hover:text-blue-400"
+              href="https://www.linkedin.com/in/aaronford/"
+            >
+              LinkedIn
+            </a>
+            <p className="text-xs" aria-hidden="true">
+              ░
+            </p>
+            <a
+              href="https://github.com/fordooo"
+              className="transition-transform-colors duration-300 hover:-rotate-3 hover:text-purple-400"
+            >
+              GitHub
+            </a>
+            <span className="text-xs" aria-hidden="true">
+              ░
+            </span>
+            <a
+              className="transition-transform-colors duration-300 hover:-rotate-3 hover:text-slate-400"
+              href="#"
+            >
+              Email
+            </a>
+          </div>
+        </div>
+        <ASCIIShadows />
+      </section>
+    </main>
+  )
 }
